cffcbc048492e54335e05cf5fb303d9fb7d496c4
umesh <rokayaumess@gmail.com>
2025-02-02 15:10:55 +0545

    changed app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-njasknfask;j'glkas/
 def webout():
 
- return '<h1>Devhas;dkhfasj;Ops is fun.</h1>'
+ return '<h1>deOps is great</h1>'
 
 app.run(host='0.0.0.0',port=7000)
