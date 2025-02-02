f8b3a550dd9caddf92bba042e9f0df87707525b7
umesh <rokayaumess@gmail.com>
2025-02-02 11:13:38 +0545

    mistake

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-
+njasknfask;j'glkas/
 def webout():
 
- return '<h1>DevOps is fun.</h1>'
+ return '<h1>Devhas;dkhfasj;Ops is fun.</h1>'
 
 app.run(host='0.0.0.0',port=7000)
